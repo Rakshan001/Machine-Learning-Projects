009056455cae90b969a9af1508d9c33554faa9e0
Rakshan001 <rakshanshetty2003@gmail.com>
2024-09-16 16:15:45 +0530

    save

---
+++ Opencv/input_output.py
@@ -0,0 +1,18 @@
+import os
+
+import cv2
+
+#read image
+image_path=os.path.join('.','img','orangecat.jpeg')
+
+img=cv2.imread(image_path)
+print(img.shape)
+
+#write image
+
+# ('current directory',"folder name",'img-name-for saving the img'),refering img which need to be saved
+cv2.imwrite(os.path.join('.','img','orangecat_out.jpeg'),img)
+
+# visulaize image
+cv2.imshow('imga',img)
+cv2.waitKey(0)
