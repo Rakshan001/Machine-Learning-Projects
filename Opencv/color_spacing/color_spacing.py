0ac7e39ea355f3f1db08713e88653458ade51fd0
Rakshan001 <rakshanshetty2003@gmail.com>
2024-09-17 07:39:14 +0530

    saving

--- Opencv/color_spacing/color_spacing.py
+++ Opencv/color_spacing/color_spacing.py
@@ -2,9 +2,12 @@ import cv2
 import os
 
 img=cv2.imread('img/orangecat.jpeg')
-cv2.imshow('image',img)
 
 rgb_img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-cv2.imshow('image',rgb_img)
+cv2.imshow('image',img)
+rgb_img=cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
+cv2.imshow('image',img)
+cv2.imshow('rgb_image',rgb_img)
+
 
 cv2.waitKey(0)
