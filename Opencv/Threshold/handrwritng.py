3ba19e0a0b2634bc7912a0886d40a03b72aed5f4
Rakshan001 <rakshanshetty2003@gmail.com>
2024-09-18 22:17:57 +0530

    save

--- Opencv/Threshold/handrwritng.py
+++ Opencv/Threshold/handrwritng.py
@@ -4,9 +4,9 @@ import os
 img=cv2.imread('img/handwritten.jpeg')
 
 gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret,thresh=cv2.threshold(gray_img,180,200,cv2.THRESH_BINARY)  #global threshold
+ret,thresh=cv2.threshold(gray_img,170,200,cv2.THRESH_BINARY)  #global threshold
 # thresh=cv2.blur(thresh,(5,53))  #remove unwanted line smothhing the img
-# ret,thresh=cv2.threshold(thresh,80,255,cv2.THRESH_BINARY)  
+# ret,thresh=cv2.threshold(thresh,180,255,cv2.THRESH_BINARY)  
 
 cv2.imshow("grayimg",gray_img)
 cv2.imshow("threshold",thresh)
